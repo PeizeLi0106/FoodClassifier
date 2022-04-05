1a8721471d185390fbfaa33777cb4d608ef11a5a
Peize Li <pl2092@rit.edu>
2022-04-04 20:37:25 -0500

    Initial Commit

---
+++ FoodClassifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodClassifier
+//
+//  Created by Peize li on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
